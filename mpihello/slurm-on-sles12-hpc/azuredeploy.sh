374aade91747df3a1bbdf40968dced21aa6b5501
Mayeul d'Avezac <m.davezac@imperial.ac.uk>
2018-05-01 11:54:23 +0100

    Not sure it's finding fdisk

--- mpihello/slurm-on-sles12-hpc/azuredeploy.sh
+++ mpihello/slurm-on-sles12-hpc/azuredeploy.sh
@@ -93,8 +93,8 @@ setup_data_disks()
 
     # Loop through and partition disks until not found
     for disk in sdc sdd sde sdf sdg sdh sdi sdj sdk sdl sdm sdn sdo sdp sdq sdr; do
-        fdisk -l /dev/$disk || break
-        fdisk /dev/$disk << EOF
+        /usr/sbin/fdisk -l /dev/$disk || break
+        /usr/sbin/fdisk /dev/$disk << EOF
 n
 p
 1
